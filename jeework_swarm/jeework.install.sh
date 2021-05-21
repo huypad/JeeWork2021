1664c9bf829891a0dfc27a7e909f2fe28630076e
hoaaq <auhoa1997@gmail.com>
2021-05-21 15:22:25 +0700

    hoaaq

--- jeework_swarm/jeework.install.sh
+++ jeework_swarm/jeework.install.sh
@@ -4,7 +4,7 @@ export DOMAIN=jee.vn
 export VAULT_ENDPOINT=http://vault_vault:8200
 export VAULT_TOKEN=s.CnO0sRfOCIvMAYcSnoceE1A5
 export KAFKA_BROKER=kafka1:9999,kafka2:9999,kafka3:9999
-export CONNECTION_STRING="Data Source=192.168.199.4,1433;Initial Catalog=jeework;User ID=jeeerp;Password=D3vDB@Je3121"
+export CONNECTION_STRING="Data Source=192.168.199.4,1433;Initial Catalog=JeeWork;User ID=jeework;Password=Jee33W0rkdB"
 export PUBLIC_LINK_API=https://jeework-api.jee.vn/
 export BACKEND_URL=https://jeework.jee.vn/
 export JEEACCOUNT_API=https://jeeaccount-api.jee.vn/
