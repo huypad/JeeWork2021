fcd068eebce88e70382d57c2ab982351d024c504
hoaaq <auhoa1997@gmail.com>
2021-06-24 18:12:43 +0700

    hoaaq

---
+++ scripts/env/prod.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export REGISTRY_HOST=registry.jee.vn
+export REGISTRY_PUBLISHER=hoaaq
+export REGISTRY_USERNAME=admin
+export REGISTRY_PASSWORD=registrypw123
+export PROJECT_NAME=jeework
+export DOMAIN=jee.vn
+
+export VAULT_ENDPOINT=http://vault_vault:8200
+export VAULT_TOKEN=s.ucWXuLsJ4pRQkp7aCuL81L6R
+export KAFKA_BROKER=kafka1:9999,kafka2:9999,kafka3:9999
+export HOST_JEEWORK_BE=https://jeework.jee.vn
+export HOST_JEEWORK_API=https://jeework-api.jee.vn
+export HOST_ACCOUNT_API=https://jeeaccount-api.jee.vn
+export HOST_CDN=cdn.jee.vn
+
+export APP_IsOnlineDB=null
+export APP_ConnectionString="Data Source=192.168.199.4,1433;Initial Catalog=jeework;User ID=jeework;Password=Jee33W0rkdB"
+export APP_SecretKey=6LfyWeYUAAAAAPMDrJF8PQ6hsO0ytRLRIc2fKgds
+export APP_Key=ZbsHPbRqNvdgMfyG
