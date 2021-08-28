93c0ccc633c222f25dd693a0574c6cccbb4779de
huypad <huytranvan1404@gmail.com>
2021-08-28 16:20:04 +0700

    check in

--- API_JeeWork2021/Classes/Automation.cs
+++ API_JeeWork2021/Classes/Automation.cs
@@ -1,5 +1,6 @@
 ﻿using DPSinfra.Kafka;
 using DPSinfra.Notifier;
+using JeeWork_Core2021.Controllers.Wework;
 using JeeWork_Core2021.Models;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
@@ -20,9 +21,13 @@ namespace API_JeeWork2021.Classes
 
         public async static void SendAutomation(Post_Automation_Model post, IConfiguration _config, IProducer _producer)
         {
-            string topic = _config.GetValue<string>("KafkaConfig:TopicProduce:JeeWorkAutomationService");
-            string obj = Newtonsoft.Json.JsonConvert.SerializeObject(post);
-            await _producer.PublishAsync(topic, obj);
+            if (WeworkLiteController.IsNotify(_config))
+            {
+                string topic = _config.GetValue<string>("KafkaConfig:TopicProduce:JeeWorkAutomationService");
+                string obj = Newtonsoft.Json.JsonConvert.SerializeObject(post);
+                await _producer.PublishAsync(topic, obj);
+            }
+                
         }
     }
 }
