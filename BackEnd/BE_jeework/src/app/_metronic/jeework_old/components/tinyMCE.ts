5147f4dee9fbc367f839f1c8dd3cc1f794dbdc74
huypad <huytranvan1404@gmail.com>
2021-12-03 09:09:42 +0700

    Config tiny mới nhất

--- BackEnd/BE_jeework/src/app/_metronic/jeework_old/components/tinyMCE.ts
+++ BackEnd/BE_jeework/src/app/_metronic/jeework_old/components/tinyMCE.ts
@@ -2,7 +2,7 @@ import { AttachmentModel, FileUploadModel } from 'src/app/pages/JeeWork_Core/pro
 import { environment } from 'src/environments/environment';
 
 const tinyMCE = {
-    // plugins: 'autoresize autosave paste print code preview searchreplace autolink directionality visualblocks visualchars fullscreen image link media  template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern help ',
+    // plugins: 'autoresize autosave paste print code preview searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern help ',
     plugins: 'link image code paste table lists preview autolink fullscreen',
     toolbar: 'fullscreen | undo redo | bold italic underline | bullist numlist outdent indent | alignleft aligncenter alignright alignjustify | forecolor backcolor image link table | removeformat code paste pastetext preview ',
     image_uploadtab: true,
@@ -10,12 +10,11 @@ const tinyMCE = {
     menubar: false,
     paste_as_text: true,
     paste_data_images: true,
-    smart_paste: true,  // Phát hiện văn bản giống với một URL và thay đổi văn bản thành siêu liên kết.	Phát hiện văn bản giống với URL của hình ảnh và sẽ cố gắng thay thế văn bản bằng hình ảnh.
+    smart_paste: true,
     resize: true,
     height: 100,
     min_height: 100,
     max_height: 850,
-    // content_style: "body { margin: 1rem auto; max-width: 900px; }",
     autoresize_bottom_margin: 25,
     autoresize_on_init: true,
     language: 'vi',
@@ -73,7 +72,6 @@ const tinyMCE = {
             freeTiny.style.display = 'none';
         }
     },
-    // setup = editor => {
     setup: function (editor) {
         editor.on('BeforeSetContent', e => {
             if (e.content && e.content.includes('blob:')) {
